10fa583a6469a462d41fd997bde3055269da5aac
MSI\Admin <101080058+sydvph19885@users.noreply.github.com>
2025-03-20 23:50:45 +0700

    update element path

--- frontend/src/routers/RouterProvider.tsx
+++ frontend/src/routers/RouterProvider.tsx
@@ -1,8 +1,9 @@
+import App from "App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const RouterProviders = () => {
   const routers = createBrowserRouter([
-    { path: "/", element: <div>home</div> },
+    { path: "/", element: <App /> },
     { path: "/login", element: <div>login</div> },
     { path: "/register", element: <div>register</div> },
     { path: "/admin", element: <div>admin</div> },
