8c2726861e08ec1cb71faae6b96fe6121b72a6ba
MSI\Admin <101080058+sydvph19885@users.noreply.github.com>
2025-03-20 23:53:30 +0700

    add config useApp antd

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,15 @@
 import { AppContext } from "@hooks/CurrentAppContext";
+import { App } from "antd";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import RouterProvider from "routers/RouterProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AppContext>
-      <RouterProvider />
-    </AppContext>
+    <App>
+      <AppContext>
+        <RouterProvider />
+      </AppContext>
+    </App>
   </StrictMode>
 );
