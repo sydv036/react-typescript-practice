c0c4efe0706612f468ff8c6f4acb9851a5b09c01
MSI\Admin <101080058+sydvph19885@users.noreply.github.com>
2025-03-22 23:48:34 +0700

    complete f5 reload user

--- frontend/src/hooks/CurrentAppContext.tsx
+++ frontend/src/hooks/CurrentAppContext.tsx
@@ -1,17 +1,37 @@
 import { ApiFetchUser } from "@services/api.auth";
 import { createContext, useContext, useEffect, useState } from "react";
+import React from "react";
+import { Spin } from "antd";
 
 const MyContext = createContext<IAppContext | null>(null);
 export const AppContext = (props: IPropsChildren) => {
   const [user, setUser] = useState<IUser | null>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const [percent, setPercent] = React.useState(0);
+
+  const showLoader = () => {
+    let ptg = -10;
+
+    const interval = setInterval(() => {
+      ptg += 5;
+      setPercent(ptg);
+
+      if (ptg > 120) {
+        clearInterval(interval);
+        setIsLoading(false);
+        setPercent(0);
+      }
+    }, 100);
+  };
 
   useEffect(() => {
     HandleFetchUser();
   }, []);
 
   const HandleFetchUser = async () => {
+    setIsLoading(true);
+    showLoader();
     const res = await ApiFetchUser();
     if (res.data) {
       setUser(res.data.user);
@@ -20,20 +40,27 @@ export const AppContext = (props: IPropsChildren) => {
       setUser(null);
       setIsAuthenticated(false);
     }
+    setIsLoading(false);
   };
   return (
-    <MyContext.Provider
-      value={{
-        user,
-        setUser,
-        isLoading,
-        setIsLoading,
-        isAuthenticated,
-        setIsAuthenticated,
-      }}
-    >
-      {props.children}
-    </MyContext.Provider>
+    <>
+      {isLoading ? (
+        <Spin spinning={isLoading} percent={percent} fullscreen />
+      ) : (
+        <MyContext.Provider
+          value={{
+            user,
+            setUser,
+            isLoading,
+            setIsLoading,
+            isAuthenticated,
+            setIsAuthenticated,
+          }}
+        >
+          {props.children}
+        </MyContext.Provider>
+      )}
+    </>
   );
 };
 
