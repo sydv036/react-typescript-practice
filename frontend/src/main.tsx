a7a301dd6e05622debe8f928a9ce5e3c1b81d8a9
MSI\Admin <101080058+sydvph19885@users.noreply.github.com>
2025-03-25 21:56:29 +0700

    update language to english table pro component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,18 @@
 import { AppContext } from "@hooks/CurrentAppContext";
-import { App } from "antd";
+import { App, ConfigProvider } from "antd";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import RouterProvider from "routers/RouterProvider.tsx";
+import enUS from "antd/lib/locale/en_US";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App>
-      <AppContext>
-        <RouterProvider />
-      </AppContext>
+      <ConfigProvider locale={enUS}>
+        <AppContext>
+          <RouterProvider />
+        </AppContext>
+      </ConfigProvider>
     </App>
   </StrictMode>
 );
